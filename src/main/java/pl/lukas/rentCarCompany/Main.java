d14cadb6bce8c9c6c963c76e882605142dd18d16
LukasKodym <lukasz.kodym@gmail.com>
2019-02-09 10:55:32 +0100

    update

--- src/main/java/pl/lukas/rentCarCompany/Main.java
+++ src/main/java/pl/lukas/rentCarCompany/Main.java
@@ -1,10 +1,24 @@
 package pl.lukas.rentCarCompany;
+
+import pl.lukas.rentCarCompany.service.RentCompanyService;
+import pl.lukas.rentCarCompany.service.RentCompanyServiceImpl;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // adding rent car company
+
+        RentCompanyService rentCompanyService = new RentCompanyServiceImpl();
+
+        // adding rentcar company
+
+        rentCompanyService.createNewCompany();
+
         // adding and removing departments
+
+        rentCompanyService.addDepartmentToCompany();
+        rentCompanyService.removeDepartmentToCompany();
+
 
     }
 
---
+++ src/main/java/pl/lukas/rentCarCompany/service/RentCompanyServiceImpl.java
@@ -0,0 +1,22 @@
+package pl.lukas.rentCarCompany.service;
+
+import pl.lukas.rentCarCompany.domain.RentCompany;
+
+public class RentCompanyServiceImpl implements RentCompanyService {
+    @Override
+    public RentCompany createNewCompany() {
+        return null;
+    }
+
+    @Override
+    public RentCompany addDepartmentToCompany() {
+
+
+        return null;
+    }
+
+    @Override
+    public RentCompany removeDepartmentToCompany() {
+        return null;
+    }
+}
