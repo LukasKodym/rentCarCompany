ed5519eb86a3476c9903572342795e31237af01f
LukasKodym <lukasz.kodym@gmail.com>
2019-02-10 18:40:24 +0100

    update RentCompanyService class

--- src/main/java/pl/lukas/rentCarCompany/service/RentCompanyService.java
+++ src/main/java/pl/lukas/rentCarCompany/service/RentCompanyService.java
@@ -1,16 +1,19 @@
 package pl.lukas.rentCarCompany.service;
 
-import pl.lukas.rentCarCompany.domain.Department;
+import pl.lukas.rentCarCompany.domain.CarStatusEnum;
+import pl.lukas.rentCarCompany.domain.CarTypesEnum;
 import pl.lukas.rentCarCompany.domain.RentCompany;
-
-import java.util.List;
 
 public interface RentCompanyService {
 
     RentCompany createNewCompany(String name, String webDomain, String address, String owner,
                                  String logo);
-
     RentCompany handleDepartmentsFromCompany(RentCompany newCompany, String userChoice, String address);
     void addEmployeeWithParams(RentCompany newCompany, String firstName, String lastName,
                                boolean isManager, String departmentAddress);
+    void createCarForDepartment(RentCompanyServiceImpl rentCompanyService, RentCompany newCompany,
+                                String brand, String model, CarTypesEnum carTypesEnum,
+                                int productionYear, String color, int mileage, CarStatusEnum carStatusEnum,
+                                double costPerDay, String deptAddress);
+
 }
